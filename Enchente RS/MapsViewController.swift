8f4200d2fed26417969115133ff22f7164c5856c
Rodrigo Torres <rodrigohtorres@hotmail.com>
2024-05-06 16:19:31 -0300

    remove unused code

--- Enchente RS/MapsViewController.swift
+++ Enchente RS/MapsViewController.swift
@@ -10,62 +10,70 @@ import GoogleMaps
 //import GooglePlaces
 
 class MapsViewController: UIViewController, GMSMapViewDelegate {
-
+    var options: GMSMapViewOptions!
+    var mapView: GMSMapView!
     var cardView: UIView!
-        var cardTitleLabel: UILabel!
-        var cardSubtitleLabel: UILabel!
+    var cardTitleLabel: UILabel!
+    var cardSubtitleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let options = GMSMapViewOptions()
+        setupMap()
+        setupCard()
+        setupMark()
+        setupView()
+    }
+
+    func setupView() {
+        self.view.addSubview(mapView)
+    }
+
+    func setupMap() {
+        options = GMSMapViewOptions()
         options.camera = GMSCameraPosition.camera(
             withLatitude:  -30.0277, longitude: -51.2287 , zoom: 16.0)
         options.frame = self.view.bounds
 
-        let mapView = GMSMapView(options: options)
+        mapView = GMSMapView(options: options)
         mapView.delegate = self
-        self.view.addSubview(mapView)
+    }
 
+    func setupMark() {
         let marker = GMSMarker()
         marker.icon = UIImage.donation
         marker.position = CLLocationCoordinate2D(latitude:  -30.0277, longitude: -51.2287)
         marker.title = "Porto Alegre"
         marker.snippet = "Rio Grande do Sul"
         marker.map = mapView
-
-        setupCardView()
     }
 
-        func setupCardView() {
-            let cardHeight: CGFloat = 120
-            let screenWidth = UIScreen.main.bounds.width
-            let screenHeight = UIScreen.main.bounds.height
+    func setupCard() {
+        let cardHeight: CGFloat = 120
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
 
-            cardView = UIView(frame: CGRect(x: 0, y: screenHeight, width: screenWidth, height: cardHeight))
-            cardView.backgroundColor = .white
-            view.addSubview(cardView)
+        cardView = UIView(frame: CGRect(x: 0, y: screenHeight, width: screenWidth, height: cardHeight))
+        cardView.backgroundColor = .white
+        view.addSubview(cardView)
 
-            cardTitleLabel = UILabel(frame: CGRect(x: 20, y: 10, width: screenWidth - 40, height: 30))
-            cardTitleLabel.font = UIFont.boldSystemFont(ofSize: 20)
-            cardTitleLabel.textColor = .black
-            cardView.addSubview(cardTitleLabel)
+        cardTitleLabel = UILabel(frame: CGRect(x: 20, y: 10, width: screenWidth - 40, height: 30))
+        cardTitleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        cardTitleLabel.textColor = .black
+        cardView.addSubview(cardTitleLabel)
 
-            cardSubtitleLabel = UILabel(frame: CGRect(x: 20, y: 40, width: screenWidth - 40, height: 60))
-            cardSubtitleLabel.numberOfLines = 0
-            cardSubtitleLabel.textColor = .black
-            cardView.addSubview(cardSubtitleLabel)
-        }
-
-        // Método do GMSMapViewDelegate chamado quando um marcador é tocado
-        func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
-            // Exibir o cardView na parte inferior da tela
-            UIView.animate(withDuration: 0.3) {
-                self.cardView.frame.origin.y = UIScreen.main.bounds.height - self.cardView.frame.height-100
-            }
-
-            cardTitleLabel.text = marker.title
-            cardSubtitleLabel.text = marker.snippet
-
-            return true
-        }
+        cardSubtitleLabel = UILabel(frame: CGRect(x: 20, y: 40, width: screenWidth - 40, height: 60))
+        cardSubtitleLabel.numberOfLines = 0
+        cardSubtitleLabel.textColor = .black
+        cardView.addSubview(cardSubtitleLabel)
     }
+
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        UIView.animate(withDuration: 0.3) {
+            self.cardView.frame.origin.y = UIScreen.main.bounds.height - self.cardView.frame.height-100
+        }
+
+        cardTitleLabel.text = marker.title
+        cardSubtitleLabel.text = marker.snippet
+        return true
+    }
+}
